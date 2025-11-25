45e899218d97fb921ec4071bf5ebc1adada72317
Gabrielgit04 <gabrielmoises1202@gmail.com>
2025-11-25 18:44:20 -0400

    haciendo el registro de civiles

--- views/recover-password/index.php
+++ views/recover-password/index.php
@@ -1,4 +1,5 @@
-
+<?php
+session_start();?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,20 +14,11 @@
         <a href="../../views/login/index.php"><img src="../assets/imgs/icons/arrow-left.svg" alt="exit" class="exit"></a>
 
         
-        <form action="/controller/recover.php" method="post" autocomplete="off">
+        <form action="/controller/recover/recover.php" method="post" autocomplete="off">
             <header class="header-box"><h2>Recupera tu cuenta</h2></header>
 
+            <h5 class="text"><?php echo $_SESSION['q1'] ?></h5>
 
-
-            <select id="security-question" name="security-question" required>
-                <option value="" selected disabled>-- Selecciona una pregunta --</option>
-                <option value="rol-consejo">¿Cuáll es tu cargo en el consejo comunal?</option>
-                <option value="first-job">¿Cuál fue tu primer trabajo?</option>
-                <option value="favorite-teacher">
-                    ¿Cuál era el nombre de tu maestro favorito?</option>
-                <option value="name-mom">¿Nombre de tu Mamá?</option>
-                <option value="son-daughter">¿Nombre de tu Hijo/a?</option>
-            </select>
 
             <div class="input_area">
                 <input type="text" name="answer" id="user" class="entry" placeholder="Respuesta" minlength="3" maxlength="30"  title="Se permiten letras, numeros y guines bajos, y la longitud debe ser de 3 a 30 caracteres" required>
